2c61cf9dad3a0eaa964be90cc5fe2f654a1c3047
JustinHammitt <Justin.hammitt.175@hotmail.com>
2025-04-16 12:01:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-##this file exists to hold any secrets or api key's that should not be uploaded to github
-#Proxmox server info
-PVE_NODE = "pve"
-PROXMOX_INTERNAL_IP = "192.168.85.93"
-PROXMOX_PUBLIC_IP = "162.202.3.76"
-#LDAP_REALM = "LDAP"
+
